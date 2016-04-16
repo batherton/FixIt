0ee38f07fba03c5c95eae63e03ec561411e57947
batherton <brian@asimail.com>
2016-04-16 01:15:22 -0400

    update 1
    update 1

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,21 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -11,56 +28,22 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-
-    //onSuccess: function(position) {
-	//	document.getElementById('AppLoading').style="display:none;";
-	//	document.getElementById('AppLoading').className="AppLoaded";
-	//	document.getElementById('Latitude').value = position.coords.latitude;
-	//	document.getElementById('Longitude').value = position.coords.longitude;
-	//	document.getElementById('Altitude').value = position.coords.altitude;
-	//	document.getElementById('Accuracy').value = position.coords.accuracy;
-	//	document.getElementById('AltitudeAccuracy').value = position.coords.altitudeAccuracy;
-	//	document.getElementById('Heading').value = position.coords.heading;
-	//	document.getElementById('Speed').value = position.coords.speed;
-	//	document.getElementById('Timestamp').value = position.timestamp;
-	//},
-
-	//onError: function(error) {
-		//alert('Please Enable Location Services');
-	    //alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-	//},
-
-	//OnDevicePlatformSuccess: function(){
-	//	document.getElementById('DeviePlatform').value = device.platform;
-	//},
-	//OnDevicePlatformError: function(error){
-	//		alert(error);
-	//},
-
-
+    // deviceready Event Handler
+    //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		//alert('Device Ready');
-		//navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
-		//document.getElementById('DevicePlatform').value = device.platform;
-		//document.getElementById('DeviceID').value = device.uuid;
-		//document.getElementById('DeviceModel').value = device.model;
-		//document.getElementById('DeviceVersion').value = device.version;
-		OnLoadActions();
-		//navigator.notification.vibrate();
-		app.receivedEvent('deviceready');
-
-
+        app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
+
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-        console.log('Received Event: ' + id);
 
+        console.log('Received Event: ' + id);
     }
 };
