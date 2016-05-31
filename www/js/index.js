93e63e0844adc72ece36525261a363c5e8184edc
batherton <brian@asimail.com>
2016-05-31 05:02:37 -0400

    1
    1

--- www/js/index.js
+++ www/js/index.js
@@ -49,19 +49,3 @@ var app = {
 };
 
 
-function Flashlight(){
-
-
-
-	window.plugins.flashlight.available(function(isAvailable) {
-	  if (isAvailable) {
-
-		window.plugins.flashlight.toggle();
-
-	  } else {
-	    alert("Flashlight not available on this device");
-	  }
-	});
-
-
-};
